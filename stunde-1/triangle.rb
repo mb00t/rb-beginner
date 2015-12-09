fd1a2e82d6b3cafb43d75ec03618248fa3d65ad1
mb00t <mb00t@yandex.ru>
2015-12-09 18:07:16 +0300

    add 3 files of first programm

---
+++ stunde-1/triangle.rb
@@ -0,0 +1,7 @@
+# area triangle from formula 1/2*a*h
+
+puts "Введите высоту треугольника в сантиметрах"
+trweigth = gets.chomp
+puts "Введите основание треугольника в сантиметрах"
+trbase = gets.chomp
+puts "Площадь треугольника будет равна #{ 0.5 * trweigth.to_f * trbase.to_f } см"
---
+++ stunde-1/weight.rb
@@ -0,0 +1,11 @@
+#programm optimal weigth from ugrowth data
+
+puts "Приветствую. Введите свое имя"
+uname = gets.chomp
+puts "#{ uname }, сообщите свой рост в сантиметрах"
+ugrowth = gets.chomp
+
+if ugrowth.to_i - 110 <= 0 then puts "#{uname}, ваш вес уже оптимальный"
+  else
+    puts "#{ uname }, ваш оптимальный вес должен составлять #{ ugrowth.to_i - 110 } кг"
+end
