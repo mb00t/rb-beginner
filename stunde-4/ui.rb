7214a68b4913622bcd6c0dcb27c0a0e445c2b2a8
mb00t <mb00t@yandex.ru>
2016-01-24 15:16:53 +0300

    ui problem stupid

---
+++ stunde-4/ui.rb
@@ -0,0 +1,42 @@
+require_relative 'railwaystation.rb'
+require_relative 'train.rb'
+require_relative 'route'
+require_relative 'passengerwagon.rb'
+require_relative 'cargowagon.rb'
+
+UI_B = "\x1b[0m"
+ui_a = {:red => "\x1b[31m", :blue => "\x1b[34m", :green => "\x1b[32m"}
+
+def add_station
+  # добавить станцию
+end
+
+def create_train
+  # создать поезд
+end
+
+def add_wagon
+  # добавить вагон
+end
+
+def delete_wagon
+  # отцепить вагон
+end
+
+def add_train_to
+  # поместить поезд на станцию
+end
+
+def show_stations
+  # показать список станций
+end
+
+def show_train
+  # показать поезда на станции
+end
+
+def color
+  i = "#{ ui_a[:green]} + 12"
+end
+
+color
